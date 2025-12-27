92199eb94354964919495cfaf7d1248cf50760af
Dhiraj Karangale <dakarangale02@gmail.com>
2025-12-27 16:27:47 +0530

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
-import WelcomeScreen from "@/components/WelcomeScreen";
 
 function App() {
-  const [welcomeComplete, setWelcomeComplete] = useState(false);
-
   return (
     <ThemeProvider
       attribute="class"
@@ -17,16 +13,12 @@ function App() {
       disableTransitionOnChange
     >
       <Toaster />
-      {false && !welcomeComplete ? (
-        <WelcomeScreen onWelcomeComplete={() => setWelcomeComplete(true)} />
-      ) : (
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      )}
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
